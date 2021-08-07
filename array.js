19818e61832eba30e668207ffc816d7c60b4df9a
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-07 19:45:32 +0600

    array.js updated

--- array.js
+++ array.js
@@ -7,3 +7,14 @@ var heroineCount = heroine.length;
 console.log(heroine);
 console.log(typeof heroine);
 console.log(heroineCount);
+
+// array index 
+var friendsAgeIndex = friendsAge.indexOf(13);
+console.log(friendsAgeIndex);
+var friendsAgeIndex = friendsAge.indexOf(113);  //Ans: -1
+console.log(friendsAgeIndex);
+var heroineIndex2 = heroine[1];
+console.log(heroineIndex2);
+console.log(heroine[5]);    //Ans: undefined
+friendsAge[0] = 12;
+console.log(friendsAge);
