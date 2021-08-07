57e91f43ce730c3e192bce16a30fc0eda7c98144
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-07 21:40:24 +0600

    comparison.js added

---
+++ comparison.js
@@ -0,0 +1,10 @@
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 == 6);
+console.log(5 != 6);
+
+var x = 6;
+var y = 7;
+console.log(x > y);
+console.log(x != y && 5 == 6)
+console.log(x != y || 5 == 6)
