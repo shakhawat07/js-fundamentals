7272c32b47eb7f1ffdb01bccd7e73c2f657c2879
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-07 21:59:46 +0600

    multiple-condition.js added

---
+++ multiple-condition.js
@@ -0,0 +1,10 @@
+var gotJob = true;
+var money = 250000;
+var hasHouse = false;
+
+if ((gotJob == true && money > 20000) || hasHouse == true) {
+    console.log('You are selected for marriage');
+}
+else {
+    console.log('You are not selected for marriage');
+}
