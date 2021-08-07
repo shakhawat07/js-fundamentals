b133f4dae4526eede1249b59bcf6a01ef0c87adf
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-08 01:19:29 +0600

    while-loop.js updated

--- while-loop.js
+++ while-loop.js
@@ -1,6 +1,19 @@
 var i = 0;
-
 while (i < 7) {
     console.log(i);
     i++;
+}
+
+console.log('Even Numbers');
+var x = 0;
+while (x <= 10) {
+    console.log(x);
+    x += 2;
+}
+
+console.log('Odd Numbers');
+var y = 1;
+while (y <= 10) {
+    console.log(y);
+    y += 2;
 }
