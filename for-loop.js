870739018d87690a4c319e5f096a315669462242
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-08 01:50:55 +0600

    for-loop.js updated

--- for-loop.js
+++ for-loop.js
@@ -1,3 +1,10 @@
 for (var i = 0; i < 5; i++) {
     console.log(i);
 }
+
+var arr = ['a', 'b', 'c', 'd']
+var x = 0;
+while (x < arr.length) {
+    console.log('Index ' + x + ' = ' + arr[x]);
+    x++;
+}
