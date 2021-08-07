b138c3fad89eaacbf38ee91bb6f2a8ab4b9407b5
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-07 18:26:57 +0600

    array.js added

---
+++ array.js
@@ -0,0 +1,9 @@
+var friendsAge = [11, 13, 15];
+console.log(friendsAge);
+console.log(typeof friendsAge);
+
+var heroine = ['kopila', 'sabana', 'mousomi'];
+var heroineCount = heroine.length;
+console.log(heroine);
+console.log(typeof heroine);
+console.log(heroineCount);
