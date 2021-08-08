7cc2a5871602003df96c34ad4bebfc363ec64076
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-08 13:38:25 +0600

    function.js updated

--- function.js
+++ function.js
@@ -7,3 +7,13 @@ function add() {
 // call the function 
 add();
 
+// function with parameter  
+function showMoney(moneyParameter) {
+    console.log('Money = ', moneyParameter);
+    var unitPrice = 10;
+    var unitQuantity = moneyParameter / unitPrice;
+    return unitQuantity;
+}
+var money = 200;
+var quantity = showMoney(money);
+console.log('Quantity = ', quantity)
