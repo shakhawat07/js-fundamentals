0c114e2989986ff22783fc00c407e0cc24a40e7a
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-08 13:51:53 +0600

    function.js parameter added

--- function.js
+++ function.js
@@ -17,3 +17,17 @@ function showMoney(moneyParameter) {
 var money = 200;
 var quantity = showMoney(money);
 console.log('Quantity = ', quantity)
+
+// multiple parameter in function 
+function addTwoNumbers(num1, num2) {
+    var addition = num1 + num2;
+    return addition;
+}
+function multiplyTwoNumbers(num1, num2) {
+    var multiplication = num1 * num2;
+    return multiplication;
+}
+var number1 = 5;
+var number2 = 10;
+console.log('Addition = ', addTwoNumbers(number1, number2))
+console.log('Multiplication = ', multiplyTwoNumbers(number1, number2))
